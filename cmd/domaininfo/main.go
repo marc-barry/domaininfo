5ccf41a6b5a443a7c771e7bebf6fd8ad56a35d72
Marc Barry <marc-barry@users.noreply.github.com>
2020-12-16 16:29:13 -0500

    Move the domaininfo command into its own package directory and outside of main.

--- cmd/domaininfo/main.go
+++ cmd/domaininfo/main.go
@@ -1,52 +1,18 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"log"
 	"os"
 
-	"github.com/marc-barry/domaininfo/pkg/dnsutil"
-	"github.com/marc-barry/domaininfo/pkg/types"
+	"github.com/marc-barry/domaininfo/pkg/cmd/domaininfo"
 )
-
-var resolver = dnsutil.NewResolver()
 
 func main() {
 	if len(os.Args) < 2 {
 		log.Fatal("Requires at least one command line argument")
 	}
 
-	domain := os.Args[1]
-
-	targets, err := dnsutil.CNAMEChain(resolver, domain)
-	if err != nil {
+	if err := domaininfo.RunCommand(os.Args[1]); err != nil {
 		log.Fatal(err)
-	}
-
-	ipv4s, err := dnsutil.IPv4List(resolver, domain)
-	if err != nil {
-		log.Fatal(err)
-	}
-	ipv6s, err := dnsutil.IPv6List(resolver, domain)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	ipv4Info, ipv6Info, asns, err := dnsutil.AddressesInfos(resolver, ipv4s, ipv6s)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if b, err := json.MarshalIndent(
-		types.DomainInfo{
-			Domain:                domain,
-			CanonicalNamesTargets: targets,
-			IPv4AddressInfo:       ipv4Info,
-			IPv6AddressInfo:       ipv6Info,
-			ASNDescriptions:       dnsutil.ASNDescriptions(resolver, asns),
-			CAAInfos:              dnsutil.CAAInfos(resolver, domain, targets),
-		}, "", "  "); err == nil {
-		fmt.Println(string(b))
 	}
 }
---
+++ pkg/cmd/domaininfo/domaininfo.go
@@ -0,0 +1,49 @@
+package domaininfo
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/marc-barry/domaininfo/pkg/dnsutil"
+	"github.com/marc-barry/domaininfo/pkg/types"
+)
+
+// RunCommand runs the domaininf command
+func RunCommand(domain string) error {
+	resolver := dnsutil.NewResolver()
+
+	targets, err := dnsutil.CNAMEChain(resolver, domain)
+	if err != nil {
+		return err
+	}
+
+	ipv4s, err := dnsutil.IPv4List(resolver, domain)
+	if err != nil {
+		return err
+	}
+	ipv6s, err := dnsutil.IPv6List(resolver, domain)
+	if err != nil {
+		return err
+	}
+
+	ipv4Info, ipv6Info, asns, err := dnsutil.AddressesInfos(resolver, ipv4s, ipv6s)
+	if err != nil {
+		return err
+	}
+
+	b, err := json.MarshalIndent(
+		types.DomainInfo{
+			Domain:                domain,
+			CanonicalNamesTargets: targets,
+			IPv4AddressInfo:       ipv4Info,
+			IPv6AddressInfo:       ipv6Info,
+			ASNDescriptions:       dnsutil.ASNDescriptions(resolver, asns),
+			CAAInfos:              dnsutil.CAAInfos(resolver, domain, targets),
+		}, "", "  ")
+	if err != nil {
+		return err
+	}
+	fmt.Println(string(b))
+
+	return nil
+}
